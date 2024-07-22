843c5508fab64a04992f504e25dd93365e8b66b6
tky230217 <tky2302107@stu.o-hara.ac.jp>
2024-07-22 15:52:39 +0900

    SavePictureDao一部編集

--- Kelly/src/main/java/dao/SavePictureDao.java
+++ Kelly/src/main/java/dao/SavePictureDao.java
@@ -1,16 +1,7 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import bean.Account;
-
 public class SavePictureDao extends Dao{
-	public Account get(String accountId) throws Exception {
-
-	}
+	
 
 	
 }
