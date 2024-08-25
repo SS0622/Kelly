b30d94522c4b1eccb2b1c8ba6fca4ca3fc1983d2
SS0622 <tky2302009@stu.o-hara.ac.jp>
2024-08-25 10:50:37 +0900

    Merge branch 'master' of https://github.com/SS0622/Kelly.git

--- Kelly/src/main/java/main/MainAction.java
+++ Kelly/src/main/java/main/MainAction.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.util.ArrayList;
+
+import bean.Post;
+import dao.PostDao;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -10,9 +14,17 @@ public class MainAction extends Action {
 	public void execute(HttpServletRequest req,HttpServletResponse res)throws Exception{
 		HttpSession session = req.getSession();
 		
+		PostDao postDao = new PostDao();
+		ArrayList<Post> postList = new ArrayList<Post>();
+		ArrayList<String> tag = new ArrayList<String>();
+		
+		postList = postDao.search(tag);
+		
 		String url = "";
 		
 		url = "main.jsp";
+		
+		req.setAttribute("postList", postList);
 		
 		req.getRequestDispatcher(url).forward(req, res);
 	}
