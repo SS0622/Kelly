81bb732d46f43deeeb8acd30ef98c4642595a113
tky230217 <tky2302107@stu.o-hara.ac.jp>
2024-08-26 13:16:52 +0900

    memogaki

--- Kelly/src/main/java/main/PostFile.java
+++ Kelly/src/main/java/main/PostFile.java
@@ -54,7 +54,7 @@ public class PostFile extends HttpServlet {
 		
 		// ファイルの保存先
 		Path uploadPath = Paths.get("C:/Users/ebinuma/git/Kelly2/Kelly/src/main/webapp/upload");// C:/upload
-		
+									// /home/lg/tomcat/tomcat10/webapps/Kelly/upload
         // 保存先ディレクトリが存在しない場合は作成
         if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
